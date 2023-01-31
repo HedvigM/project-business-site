052c0ff822781db814c4ab9c11efffbe94b40e32
Hedvig Mejstedt <hedvig@mejstedt.se>
2023-01-31 20:39:46 +0100

    Update script.js

--- code/script.js
+++ code/script.js
@@ -1,10 +1,18 @@
 // Accordion
+// toggle() är en inbyggd i funktion i javascript som gör att vi kan toggla klassnamnet innuti parentesen.
 function toggle() {
-  this.classList.toggle("active")
+  this.classList.toggle('active');
 }
 
-document.getElementById("section1").onclick = toggle
+// getElementByClassName tar in flera klasser till skillnad från getElementBuId som endast tar in ett värde.
+const question = document.getElementsByClassName('question-accordion');
+
+/* document.getElementById("section1").onclick = toggle
 document.getElementById("section2").onclick = toggle
 document.getElementById("section3").onclick = toggle
 document.getElementById("section4").onclick = toggle
-document.getElementById("section5").onclick = toggle
+document.getElementById("section5").onclick = toggle */
+
+for (let i = 0; i < question.length; i++) {
+  question[i].onclick = toggle;
+}
